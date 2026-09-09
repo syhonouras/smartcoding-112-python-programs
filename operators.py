22295ffcee965ea4b7fa6dec32f98db49c8b4666
syhonouras <norahsyeda0@gmail.com>
2026-09-09 15:17:23 +0530

    Fix arithmetic operations and calculations in operators.py
    Fixed variable names and added missing calculations for area and circumference. Corrected function definitions and improved arithmetic operations.

--- operators.py
+++ operators.py
@@ -1,1 +1,36 @@
+#arithmetic operators
+#1-read 2 numbers and perform arithmetic operations on them
+a=5, b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#2-calculate the area of a circle given radius (A = π*r^2)
+import math
+def calcucalte(radius=2.0):
+  area = maths.pi * radius**2
+  circumerence + 2 * math.pi * radius
+  return area, circumference
+
+#3-calculate simple interest (P X T X R)/100
+#using function
+def fun(P, T, R):
+  return(p * T * R)/100
+
+p, t, r = 8, 6, 10
+res = (p, t, r)
+print(res)
+
+#using lambda function
+si = lambda p, t, r:(p * t * r)/100
+p, t, r = 8, 6, 10
+res = si(p, t, r)
+print(res)
+
+#using list comprehensions
+
 
